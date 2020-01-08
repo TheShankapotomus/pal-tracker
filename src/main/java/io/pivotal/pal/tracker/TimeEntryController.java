b9ea335717b506fee762861305cf18201e85729c
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:34 -0500

    Lab 7: Health Monitoring

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,7 +1,11 @@
 package io.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.*;
@@ -11,16 +15,27 @@ import static org.springframework.http.ResponseEntity.*;
 public class TimeEntryController {
 
     private TimeEntryRepository repository;
+    private final DistributionSummary timeEntrySummary;
+    private final Counter actionCounter;
 
-    public TimeEntryController(TimeEntryRepository repository){
+    public TimeEntryController(TimeEntryRepository repository,
+                               MeterRegistry meterRegistry
+    ){
 
         this.repository = repository;
+
+        timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
     }
 
     @PostMapping
     public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry entry){
 
         TimeEntry record = repository.create(entry);
+
+        actionCounter.increment();
+        timeEntrySummary.record(repository.list().size());
+
         return created(null).body(record);
     }
 
@@ -28,27 +43,46 @@ public class TimeEntryController {
     public ResponseEntity<TimeEntry> read(@PathVariable long id){
 
         TimeEntry record = repository.find(id);
-        return record != null ? ok().body(record) : notFound().build();
+
+        if (record != null) {
+
+            actionCounter.increment();
+            return ok().body(record);
+        } else {
+
+            return notFound().build();
+        }
     }
 
     @PutMapping("{id}")
     public ResponseEntity<TimeEntry> update(@PathVariable long id, @RequestBody TimeEntry entry){
 
         TimeEntry record = repository.update(id, entry);
-        return record != null ? ok().body(record) : notFound().build();
+
+        if (record != null) {
+
+            actionCounter.increment();
+            return ok().body(record);
+        } else {
+
+            return notFound().build();
+        }
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<TimeEntry> delete(@PathVariable long id){
 
         repository.delete(id);
+        actionCounter.increment();
+        timeEntrySummary.record(repository.list().size());
+
         return noContent().build();
     }
 
     @GetMapping
     public ResponseEntity<List<TimeEntry>> list(){
 
-        List<TimeEntry> records = repository.list();
-        return ok().body(records);
+        actionCounter.increment();
+        return ok().body(repository.list());
     }
 }
